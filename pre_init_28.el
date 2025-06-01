ba23493777f25e54024a0dd77ef423ef79e8713c
Wan Ardie <wan_ahmad_ardie@yahoo.com>
2025-06-01 10:06:20 +0800

    init 29 so far good

--- pre_init_28.el
+++ pre_init_28.el
@@ -1,16 +1,31 @@
 (set-background-color "LemonChiffon")
-(progn
-  (custom-set-faces)
-  (setq user-emacs-directory "~/.config/emacs/")
-  ;;(add-to-list 'load-path (expand-file-name "elpa" user-emacs-directory))
-  (setq package-user-dir "~/.config/emacs/elpa")
+(if (< emacs-major-version 29)
+    (progn
+      (custom-set-faces)
+      (setq user-emacs-directory "~/.config/emacs/")
+      ;;(add-to-list 'load-path (expand-file-name "elpa" user-emacs-directory))
+      (setq package-user-dir "~/.config/emacs/elpa")
 
-  (let ((default-directory "~/.config/emacs/elpa"))
-    (normal-top-level-add-subdirs-to-load-path))
-  ;; (custom-set-variables
-  ;;  '(package-selected-packages
-  ;;    '(magik-mode doom-themes modus-themes auto-complete multiple-cursors powershell marginalia vertico use-package)))
-  
+      (let ((default-directory "~/.config/emacs/elpa"))
+	(normal-top-level-add-subdirs-to-load-path))
+      ;; (custom-set-variables
+      ;;  '(package-selected-packages
+      ;;    '(magik-mode doom-themes modus-themes auto-complete multiple-cursors powershell marginalia vertico use-package)))
+      
 
-  (load-file "~/.config/emacs/init_28.el")
+      (load-file "~/.config/emacs/init_28.el"))
+  (progn
+
+    (custom-set-faces)
+    (setq user-emacs-directory "~/my-emacs-29-config")
+    ;;(add-to-list 'load-path (expand-file-name "elpa" user-emacs-directory))
+    (setq package-user-dir "~/my-emacs-29-config/elpa")
+    
+    (let ((default-directory "~/my-emacs-29-config/elpa"))
+      (normal-top-level-add-subdirs-to-load-path))
+ 
+
+    ;; (load-file "~/.config/emacs/init_28.el")
+    (message "asd"))
+    
   )
